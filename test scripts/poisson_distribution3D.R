7afd213d4cc25b0aa083ec9fc4912c8453f196f5
dave.le23 <david.le@petermac.org>
2024-03-06 09:44:33 +1100

    Added test scripts folder (to test draft scripts). Includes script for generating background cells via poisson's distribution.

---
+++ test scripts/poisson_distribution3D.R
@@ -0,0 +1,47 @@
+  
+poisson_distribution3D <- function(n_cells, length, width, height, cell_type = "Others")  {
+  
+  # Choose lambda
+  lambda <- 5
+  
+  # Set number of rows, columns and layers
+  nRows <- nCols <- nLays <- round((n_cells/lambda)^(1/3))
+  
+  # Get number of cubes in grid
+  nCubes <- nRows * nCols * nLays
+
+  # Get pois vector
+  pois <- rpois(nCubes, lambda)
+  
+  
+  # Get points for each square region
+  x <- c()
+  y <- c()
+  z <- c()
+  
+  for (row in seq(nRows)) {
+    
+    for (col in seq(nCols)) {
+      
+      for (lay in seq(nLays)) {
+        current_cube_index <- nRows^2 * (row - 1) + nCols * (col - 1) + lay
+        
+        x <- append(x, runif(pois[current_cube_index], row - 1, row))
+        y <- append(y, runif(pois[current_cube_index], col - 1, col))
+        z <- append(z, runif(pois[current_cube_index], lay - 1, lay))
+        
+      }
+    }
+  }
+  x <- x * length / nRows
+  y <- y * width / nCols
+  z <- z * height / nLays
+  
+  plot3d(x, y, z, size = 4, col = "lightgray")
+  
+  df <- data.frame("Cell.X.Position" = x,
+                   "Cell.Y.Position" = y,
+                   "Cell.Z.Position" = z,
+                   "Cell.Type" = cell_type)
+  return(df)
+}
