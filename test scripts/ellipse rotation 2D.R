6e64ed1b619682a0c0dfbfbf90b8e55d08d767fc
dave.le23 <david.le@petermac.org>
2024-03-07 21:30:47 +1100

    Slightly cleaned code (more readable, but still need to know the math to know). Also added a red dot in the middle of the ellipse for reference.

--- test scripts/ellipse rotation 2D.R
+++ test scripts/ellipse rotation 2D.R
@@ -2,21 +2,29 @@ length <- 20
 width <- 20
 n_points <- 1000
 
-x_radius <- 10
-y_radius <- 15
-centre_loc <- c(0, 0)
-theta <- 3 * pi / 8
+x_radius <- 15
+y_radius <- 10
+centre_loc <- c(5, 10)
+theta <- 3 * pi / 16
 
 df <- data.frame(x = runif(n_points, -length, length), 
                  y = runif(n_points, -width, width), 
                  color = "lightgray")
 
 for (i in seq(n_points)) {
-  D <- ((cos(theta) * df$x[i] + sin(theta) * df$y[i] - centre_loc[1])/x_radius)^2 + 
-       ((-sin(theta) * df$x[i] + cos(theta) * df$y[i] - centre_loc[2])/y_radius)^2
+  x <- df$x[i] - centre_loc[1]
+  y <- df$y[i] - centre_loc[2]
+  
+  D <- ((cos(theta) * x + sin(theta) * y)/x_radius)^2 + 
+       ((-sin(theta) * x + cos(theta) * y)/y_radius)^2
   if (D <= 1) {
     df$color[i] <- "blue"
   }
 }
 
+df[n_points + 1, ] <- list(centre_loc[1],
+                           centre_loc[2],
+                           "red")
+
 plot(df$x, df$y, pch = 19, col = df$color)
+
