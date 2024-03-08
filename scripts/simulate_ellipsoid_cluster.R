99a7a3883734dd1484d2ed976a23403e5904ff72
dave.le23 <david.le@petermac.org>
2024-03-08 12:17:59 +1100

    Added rotation to ellipsoid using super hard math.

--- scripts/simulate_ellipsoid_cluster.R
+++ scripts/simulate_ellipsoid_cluster.R
@@ -1,6 +1,6 @@
 simulate_ellipsoid_cluster <- function(bg_sample, cluster_properties) {
   
-  # Get sphere properties
+  # Get ellipsoid properties
   cell_type <- cluster_properties$name_of_cluster_cell
   infiltration_types <- cluster_properties$infiltration_types
   infiltration_proportions <- cluster_properties$infiltration_proportions
@@ -9,22 +9,40 @@ simulate_ellipsoid_cluster <- function(bg_sample, cluster_properties) {
   z_radius <- cluster_properties$z_radius
   centre_loc <- cluster_properties$centre_loc
   
+  theta <- cluster_properties$y_z_rotation # in x-axis
+  alpha <- cluster_properties$x_z_rotation # in y-axis
+  beta  <- cluster_properties$x_y_rotation # in z-axis
+  
+  a <- cos(alpha) * cos(beta)
+  b <- cos(alpha) * sin(beta)
+  c <- sin(alpha)
+  d <- -sin(theta) * sin(alpha) * cos(beta) - cos(theta) * sin(beta)
+  e <- -sin(theta) * sin(alpha) * sin(beta) + cos(theta) * cos(beta)
+  f <- sin(theta) * cos(alpha)
+  g <- -cos(theta) * sin(alpha) * cos(beta) + sin(theta) * sin(beta)
+  h <- -cos(theta) * sin(alpha) * sin(beta) - sin(theta) * cos(beta)
+  i <- cos(theta) * cos(alpha)
+  
+  
   # Get number of cells
   n_cells <- nrow(bg_sample)
   
-  for (i in seq_len(n_cells)) {
+  for (index in seq_len(n_cells)) {
     # Get x, y, z and phenotype of ith cell
-    x <- bg_sample[i, "Cell.X.Position"]
-    y <- bg_sample[i, "Cell.Y.Position"]
-    z <- bg_sample[i, "Cell.Z.Position"]
-    pheno <- bg_sample[i, "Cell.Type"]
+    x <- bg_sample[index, "Cell.X.Position"] - centre_loc[1]
+    y <- bg_sample[index, "Cell.Y.Position"] - centre_loc[2]
+    z <- bg_sample[index, "Cell.Z.Position"] - centre_loc[3]
+    pheno <- bg_sample[index, "Cell.Type"]
     
+    x_new <- a * x + b * y + c * z
+    y_new <- d * x + e * y + f * z
+    z_new <- g * x + h * y + i * z
     
-    D <- ((x - centre_loc[1])/x_radius)^2 + 
-         ((y - centre_loc[2])/y_radius)^2 + 
-         ((z - centre_loc[3])/z_radius)^2
+    D <- (x_new/x_radius)^2 + 
+         (y_new/y_radius)^2 + 
+         (z_new/z_radius)^2
     
-    if (D < 1){ 
+    if (D <= 1) { 
       # in the region of cluster, generate random number to decide the `Cell.Type`
       random <- stats::runif(1)
       
@@ -36,7 +54,7 @@ simulate_ellipsoid_cluster <- function(bg_sample, cluster_properties) {
       # pheno will be the corresponding infiltraiton type
       n <- 1 # start from the first proportion
       current_p <- 0
-      while (n <= n_infiltration_types){
+      while (n <= n_infiltration_types) {
         current_p <- current_p + infiltration_proportions[n]
         if (random <= current_p) {
           pheno <- infiltration_types[n]
@@ -45,7 +63,7 @@ simulate_ellipsoid_cluster <- function(bg_sample, cluster_properties) {
         n <- n+1
       }
     }
-    bg_sample[i, "Cell.Type"] <- pheno
+    bg_sample[index, "Cell.Type"] <- pheno
   }
   return(bg_sample)
 }
