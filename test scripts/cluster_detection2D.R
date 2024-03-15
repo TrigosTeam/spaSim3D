819f9413f2d79c9ad53708ac01eded7c7f082e35
dave.le23 <david.le@petermac.org>
2024-03-15 15:49:21 +1100

    Added basic cluster detection algorithm, it will find rectangles which have a high enough proportion of cells (but recursive part has not been added, yet).

--- test scripts/cluster_detection2D.R
+++ test scripts/cluster_detection2D.R
@@ -5,25 +5,24 @@ width <- 2000
 ## Define grid dimensions
 nrows <- 8
 ncols <- 8
-ngrid_squares <- nrows * ncols
+ngrid_rect <- nrows * ncols
 
-grid_square_length <- length / nrows
-grid_square_width <- width / ncols
+grid_rect_length <- length / nrows
+grid_rect_width <- width / ncols
 
 
 ### Algorithm to see proportion of cells in each grid square, show as histogram
 data <- bgCluster
 
-data <- determine_grid_square_nums(data = data,
-                                   length = length, width = width,
-                                   nrows = nrows, ncols = ncols)
+data <- determine_grid_rect_nums(data = data,
+                                 length = length, width = width,
+                                 nrows = nrows, ncols = ncols)
 
-cell_props <- find_grid_square_props(data = data,
-                                     nrows = nrows, ncols = ncols)
-cell_dens <- find_grid_square_densities(data = data,
-                                        nrows = nrows, ncols = ncols,
-                                        length = grid_square_length,
-                                        width = grid_square_width)
+cell_props <- find_grid_rect_props(data = data,
+                                   nrows = nrows, ncols = ncols)
+cell_dens <- find_grid_rect_densities(data = data,
+                                      nrows = nrows, ncols = ncols,
+                                      area = grid_rect_length * grid_rect_width)
 
 
 
@@ -42,26 +41,68 @@ plot_cells(data = data_specific_cell, length = length, width = width, nrows = nr
 
 
 
-
 ## Other stuff
-summary(cell_props[["Tumour"]][cell_props[["Tumour"]] > 0.0])
-hist(cell_props[["Tumour"]][cell_props[["Tumour"]] > 0.0])
+cell_type <- "Tumour"
+summary(cell_props[[cell_type]][cell_props[[cell_type]] > 0.0])
+hist(cell_props[[cell_type]][cell_props[[cell_type]] > 0.0])
 
-summary(cell_dens[["Tumour"]][cell_dens[["Tumour"]] > 0.0])
+summary(cell_dens[[cell_type]][cell_dens[[cell_type]] > 0.0])
+
+
+## The cluser detection function
+
+rect_data <- list()
+rect_data[[length(rect_data) + 1]] <- c()
+
+cell_type <- "Tumour"
+
+for (grid_rect in seq(ngrid_rect)) {
+  
+  if (cell_props[[cell_type]][grid_rect] > 0.2) {
+    x_coord <- (grid_rect - 1)%%ncols * grid_rect_width
+    y_coord <- floor((grid_rect - 1)/nrows) * grid_rect_length
+    
+    print(paste(x_coord, y_coord))
+    rect_data[[length(rect_data) + 1]] <- check_grid_rect(data,
+                                                          cell_type,
+                                                          x_coord,
+                                                          y_coord,
+                                                          grid_rect_length,
+                                                          grid_rect_width)
+  }
+}
 
 
 
-## Plot squares
-x <- c(15, 4, 3)
-y <- c(8, 2, 10)
-side_length <- c(2, 4, 6)
-squares_data <- data.frame(x = x, y = y, side_length = side_length)
+# bottom left coord of rectangle: (x_coord, y_coord)
 
-plot_squares(squares_data = squares_data,
-             xmin = 0,
-             xmax = 20,
-             ymin = 0,
-             ymax = 20)
+check_grid_rect <- function(data, cell_type, x_coord, y_coord, length, width) {
+  
+  data_temp <- data$Cell.Type[data$Cell.X.Position >= x_coord &
+                              data$Cell.X.Position < (x_coord + width) &
+                              data$Cell.Y.Position >= y_coord &
+                              data$Cell.Y.Position < (y_coord + length)]
+  
+  cell_prop <- (sum(data_temp == cell_type)) / length(data_temp)
+  
+  if (cell_prop > 0.9) {
+    return (c(x_coord, y_coord, length, width))
+  }
+
+  else {
+    return (c())
+  }
+}
+
+
+## Plot rectangles
+
+
+plot_rect(rect_data = rect_data,
+          xmin = 0,
+          xmax = width,
+          ymin = 0,
+          ymax = length)
 
 
 
@@ -69,9 +110,9 @@ plot_squares(squares_data = squares_data,
 
 ### Functions
 
-determine_grid_square_nums <- function(data,
-                                       length, width,
-                                       nrows, ncols) {
+determine_grid_rect_nums <- function(data,
+                                     length, width,
+                                     nrows, ncols) {
   
   
   data$Cell.Num <- ncols * floor(data$Cell.Y.Position / (length / nrows)) +
@@ -81,8 +122,8 @@ determine_grid_square_nums <- function(data,
 }
 
 
-find_grid_square_props <- function(data, 
-                                   nrows, ncols) {
+find_grid_rect_props <- function(data, 
+                                 nrows, ncols) {
 
   cell_types <- unique(data$Cell.Type)
   cell_props <- vector(mode = 'list', length = length(cell_types))
@@ -101,9 +142,9 @@ find_grid_square_props <- function(data,
 }
 
 
-find_grid_square_densities <- function(data, 
-                                       nrows, ncols,
-                                       length, width) {
+find_grid_rect_densities <- function(data, 
+                                     nrows, ncols,
+                                     area) {
   
   cell_types <- unique(data$Cell.Type)
   cell_dens <- vector(mode = 'list', length = length(cell_types))
@@ -113,7 +154,7 @@ find_grid_square_densities <- function(data,
     cells <- data$Cell.Type[data$Cell.Num == i]
     
     for (cell_type in cell_types) {
-      cell_dens[[cell_type]] <- append(cell_dens[[cell_type]], sum(cells == cell_type) / (length * width)) 
+      cell_dens[[cell_type]] <- append(cell_dens[[cell_type]], sum(cells == cell_type) / area) 
       
     }
   }  
@@ -125,24 +166,27 @@ find_grid_square_densities <- function(data,
 plot_cells <- function(data, length, width, nrows, ncols) {
   
   ggplot(data = data,
-         mapping = aes(x = Cell.X.Position, y = Cell.Y.Position, color = Cell.Type)) + 
+         aes(x = Cell.X.Position, y = Cell.Y.Position, color = Cell.Type)) + 
     geom_point() + 
     theme(
       panel.background = element_rect(fill = NA),
       panel.ontop = TRUE,
-      panel.grid.major=element_line(color = "black"),
-      panel.grid.minor=element_line(color = "black")
+      panel.grid.major = element_line(color = "black"),
+      panel.grid.minor = element_blank()
     ) + 
-    scale_x_continuous(minor_breaks = seq(0, width, width/ncols), limits = c(0, length)) +
-    scale_y_continuous(minor_breaks = seq(0, length, length/nrows), limits = c(0, width)) 
-  
+    scale_x_continuous(limits = c(0, width), breaks = round(seq(0, width, width/ncols))) +
+    scale_y_continuous(limits = c(0, length), breaks = round(seq(0, length, length/nrows))) 
 }
 
-plot_squares <- function(squares_data, xmin, xmax, ymin, ymax) {
+
+plot_rect <- function(rect_data, xmin, xmax, ymin, ymax) {
   
-  ggplot(squares_data, aes(x = x, y = y)) +
+  df <- do.call(rbind.data.frame, rect_data)
+  colnames(df) <- c("x", "y", "length", "width")
+  
+  ggplot(df, aes(x = x, y = y)) +
     geom_rect(aes(xmin = x, ymin = y, 
-                  xmax = x + side_length, ymax = y + side_length),
+                  xmax = x + width, ymax = y + length),
               fill = "green",
               color = "black") +
     xlim(xmin, xmax) +
