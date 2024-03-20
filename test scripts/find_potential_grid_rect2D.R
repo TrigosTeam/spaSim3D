1a91dd97c39d8dfcd87c6dc065097b07c71c9754
dave.le23 <david.le@petermac.org>
2024-03-20 16:43:09 +1100

    Trying to make code that can determine the rough location of particular clusters.

---
+++ test scripts/find_potential_grid_rect2D.R
@@ -0,0 +1,230 @@
+library(spaSim)
+
+### Simulate 2D data ----------------------------------------------------------
+cluster_prop <- list(
+  C1 = list(
+    name_of_cluster_cell = "Tumour",
+    size = 500,
+    shape = "Oval",
+    centre_loc = data.frame("x" = 700, "y" = 800),
+    infiltration_types = c("Immune1", "Others"),
+    infiltration_proportions = c(0.10, 0.00)),
+  C2 = list(
+    name_of_cluster_cell = "Tumour",
+    size = 400,
+    shape = "Circle",
+    centre_loc = data.frame("x" = 1500, "y" = 1500),
+    infiltration_types = c("Immune1", "Others"),
+    infiltration_proportions = c(0.5, 0.00)),
+  C3 = list(
+    name_of_cluster_cell = "Immune1",
+    size = 250,
+    shape = "Circle",
+    centre_loc = data.frame("x" = 700, "y" = 700),
+    infiltration_types = c("Tumour"),
+    infiltration_proportions = c(0.4)
+  )
+)
+
+bgCluster <- simulate_clusters(bg_sample = bgHardcore,
+                               n_clusters = 3,
+                               bg_type = "Others",
+                               cluster_properties = cluster_prop,
+                               plot_image = T,
+                               plot_categories = c("Others", "Tumour", "Immune1"),
+                               plot_colours = NULL)
+
+data <- bgCluster
+
+### Algorithm -----------------------------------------------------------------
+
+## Dimensions of study area
+length <- 2000
+width <- 2000
+
+## Define grid dimensions
+nrows <- 8
+ncols <- 8
+ngrid_rect <- nrows * ncols
+
+grid_rect_length <- length / nrows
+grid_rect_width <- width / ncols
+
+## Determine grid numbers for data
+data <- determine_grid_rect_nums(data = data,
+                                 length = length, width = width,
+                                 nrows = nrows, ncols = ncols)
+
+
+## Plot all cells
+library(ggplot2)
+plot_cells(data = data, length = length, width = width, nrows = nrows, ncols = ncols)
+
+
+## Plot only showing a specific cell type
+cell_type <- "Tumour"
+data_specific_cell <- data[data$Cell.Type == cell_type, ]
+
+plot_cells(data = data_specific_cell, length = length, width = width, nrows = nrows, ncols = ncols)
+
+
+
+
+
+## Step 1. Create vector with number for each grid_rect
+grid_rect_nums <- seq(ngrid_rect)
+
+
+## Step 2. Determine cell proportions for each cell_type for each grid_rect
+cell_props <- find_grid_rect_props(data = data,
+                                   nrows = nrows, ncols = ncols)
+
+
+## Step 3. Remove numbers from grid_rect_nums where cell_prop = 0
+cell_type <- "Tumour"
+grid_rect_nums <- grid_rect_nums[cell_props[[cell_type]] > 0]
+
+
+
+## Step 4. Determine grid_rect with highest cell_prop
+max_rect_num <- find_max_prop_grid_rect(cell_props = cell_props,
+                                    grid_rect_nums = grid_rect_nums,
+                                    cell_type = cell_type)
+
+max_rect_prop <- cell_props[[cell_type]][rect_num]
+
+
+## Step 5. Remove rect_num from grid_rect_nums vectors and add it to a new vector
+grid_rect_nums <- grid_rect_nums[-c(rect_num)]
+
+
+## Step 6. Recursive algorithm. Determine if adjacent grid_rects have a
+#          cell_prop > 25% of max_rect_prop. If they do, remove from
+#          grid_rect_nums and append to cluster_rect_nums.
+
+# cluster_rect_nums <- c(max_rect_num)
+cluster_rect_nums <- c()
+
+cluster_rect_nums <- append(cluster_rect_nums,
+                            check_adjacent_grid_rects(c(), max_rect_num, grid_rect_nums))
+
+
+
+check_adjacent_grid_rects <- function(answer, curr_grid_rect_num, grid_rect_nums) {
+  
+  if (curr_grid_rect_num %in% grid_rect_nums == FALSE) {
+    return (c())
+  }
+  
+  if (cell_props[[cell_type]][curr_grid_rect_num] > 0.25 * max_rect_prop) {
+    
+    grid_rect_nums <- grid_rect_nums[-c(curr_grid_rect_num)]
+    
+    ### CHECK LEFT, RIGHT, UP, DOWN
+    
+    ## Need to check if going left, right, up or down is possible
+    
+    # Left
+    if (curr_grid_rect_num%%ncols != 1) {
+      answer <- append(answer, 
+                       check_adjacent_grid_rects(c(), 
+                                                 curr_grid_rect_num - 1, 
+                                                 grid_rect_nums))  
+    }
+    
+    # Right
+    if (curr_grid_rect_num%%ncols != 0) {
+      answer <- append(answer, 
+                       check_adjacent_grid_rects(c(), 
+                                                 curr_grid_rect_num + 1, 
+                                                 grid_rect_nums))  
+    }
+    
+    # Up
+    if (curr_grid_rect_num <= ncols * (nrows - 1)) {
+      answer <- append(answer, 
+                       check_adjacent_grid_rects(c(), 
+                                                 curr_grid_rect_num + ncols, 
+                                                 grid_rect_nums))  
+    }
+    
+    # Down
+    if (curr_grid_rect_num > ncols) {
+      answer <- append(answer, 
+                       check_adjacent_grid_rects(c(), 
+                                                 curr_grid_rect_num - ncols, 
+                                                 grid_rect_nums))  
+    }
+    
+    answer <- append(answer, curr_grid_rect_num)
+    
+    return (answer)
+  }
+  else {
+    return (c())
+  }
+}
+
+
+
+
+
+
+
+## Functions
+determine_grid_rect_nums <- function(data,
+                                     length, width,
+                                     nrows, ncols) {
+  
+  
+  data$Cell.Num <- ncols * floor(data$Cell.Y.Position / (length / nrows)) +
+    floor(data$Cell.X.Position / (width / ncols)) + 1
+  
+  return(data)
+}
+
+plot_cells <- function(data, length, width, nrows, ncols) {
+  
+  ggplot(data = data,
+         aes(x = Cell.X.Position, y = Cell.Y.Position, color = Cell.Type)) + 
+    geom_point() + 
+    theme(
+      panel.background = element_rect(fill = NA),
+      panel.ontop = TRUE,
+      panel.grid.major = element_line(color = "black"),
+      panel.grid.minor = element_blank()
+    ) + 
+    scale_x_continuous(limits = c(0, width), breaks = round(seq(0, width, width/ncols))) +
+    scale_y_continuous(limits = c(0, length), breaks = round(seq(0, length, length/nrows))) 
+}
+
+
+
+find_grid_rect_props <- function(data, 
+                                 nrows, ncols) {
+  
+  cell_types <- unique(data$Cell.Type)
+  cell_props <- vector(mode = 'list', length = length(cell_types))
+  names(cell_props) <- cell_types
+  
+  for (i in seq(nrows * ncols)) {
+    cells <- data$Cell.Type[data$Cell.Num == i]
+    
+    for (cell_type in cell_types) {
+      
+      answer <- ifelse(length(cells) > 0, 
+                       sum(cells == cell_type) / length(cells), 0)
+      
+      cell_props[[cell_type]] <- append(cell_props[[cell_type]], answer) 
+      
+    }
+  }  
+  
+  return (cell_props)
+}
+
+
+find_max_prop_grid_rect <- function(cell_props, grid_rect_nums, cell_type) {
+  
+  return (grid_rect_nums[which.max(cell_props[[cell_type]][grid_rect_nums])])
+}
