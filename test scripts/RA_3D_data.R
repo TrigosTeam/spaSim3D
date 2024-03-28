ae4fcbf81227c5dca1af97581ddcc2ad623a3473
dave.le23 <david.le@petermac.org>
2024-03-28 14:41:31 +1100

    Changed color for 3D plot. Added legend to 3D plot.

--- test scripts/RA_3D_data.R
+++ test scripts/RA_3D_data.R
@@ -32,12 +32,20 @@ df <- data.frame(x = x,
 library(rgl)
 library("RColorBrewer")
 
+colors <- brewer.pal(n = 11, name = 'Spectral')
+
 plot3d(x = df$x,
        y = df$y,
        z = df$z,
-       col = get_colors(df$cell_type, brewer.pal(n = 11, name = 'Dark2')),
+       col = get_colors(df$cell_type, colors),
        size = 4)
-aspect3d(8, 8, 1)
+
+aspect3d(5, 5, 1)
+
+cells <- as.factor(df$cell_type)
+legend3d("topright", legend = levels(cells) , pch = 19, col = colors[seq(length(cells))], inset = c(0.02))
+
+
 
 
 get_colors <- function(groups, group.col = palette()) {
