5f43698c5d978be52b586c44cfe5ffd51214c7f2
dave.le23 <david.le@petermac.org>
2024-04-03 12:37:55 +1100

    Made function to calculate area between curves from cross-K function

---
+++ SPIAT3D scripts/Colocalization metrics/calculate_AUC_of_Kcross3D.R
@@ -0,0 +1,15 @@
+AUC_of_cross_function <- function(Kcross_df) {
+  
+  ## Get difference in area between 
+  AUC <- pracma::trapz(Kcross_df$Distance, Kcross_df$Observed) - 
+         pracma::trapz(Kcross_df$Distance, Kcross_df$Expected)
+  
+  ## Get the cross-k result image size
+  max_distance <- max(Kcross_df$Distance)
+  max_Kcross <- max(c(Kcross_df$Observed, Kcross_df$Expected))
+  
+  ## Calculate normalised AUC
+  n_AUC <- AUC / (max_distance * max_Kcross)
+  
+  return (n_AUC)
+}
