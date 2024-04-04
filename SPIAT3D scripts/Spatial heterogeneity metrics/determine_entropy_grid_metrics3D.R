00d65770b257cbbf4f6f02225ed07f250c8b5813
dave.le23 <david.le@petermac.org>
2024-04-04 18:35:01 +1100

    Finished function that divides window into smaller grid prisms and calculates entropy for each one.

--- SPIAT3D scripts/Spatial heterogeneity metrics/determine_entropy_grid_metrics3D.R
+++ SPIAT3D scripts/Spatial heterogeneity metrics/determine_entropy_grid_metrics3D.R
@@ -1,11 +1,79 @@
-### Possible function to get grid metrics for:
-## calculate_cell_proportions3D
-## calculate_entropy3D
-
-## You can use other functions, but why?
-## I don't recommend any functions which use circles because they are heavily
-## restricted by the grid rectangles
-
-determine_entropy_grid_metricx3D <- function() {
+determine_entropy_grid_metrics3D <- function(data, 
+                                             n_split,
+                                             target_cell_types,
+                                             feature_colname = "Cell.Type",
+                                             plot_image = TRUE) {
   
+  ## Get dimensions of the window
+  length <- round(max(data$Cell.X.Position) - min(data$Cell.X.Position))
+  width  <- round(max(data$Cell.Y.Position) - min(data$Cell.Y.Position))
+  height <- round(max(data$Cell.Z.Position) - min(data$Cell.Z.Position))
+  
+  ## Get distance of row, col and lay
+  d_row <- length / n_split
+  d_col <- width / n_split
+  d_lay <- height / n_split
+  
+  ## Figure out which 'grid prism number' each cell is inside
+  data$Prism.Num <- floor(data$Cell.X.Position / d_row) +
+                    floor(data$Cell.Y.Position / d_col) * n_split + 
+                    floor(data$Cell.Z.Position / d_lay) * n_split^2 + 1
+  
+  
+  ## Calculate entropy for each grid prism
+  n_grid_prisms <- n_split^3
+  cell_type_list <- vector(mode = 'list', length = length(target_cell_types))
+  grid_prism_entropies <- c()
+  
+  for (grid_prism_num in seq(n_grid_prisms)) {
+    
+    ## Get data of cells in the current grid_prism
+    data_temp <- data[data$Prism.Num == grid_prism_num, ]
+    
+    grid_prism_entropy <- calculate_entropy3D(data_temp,
+                                              target_cell_types = target_cell_types)
+    
+    ## Get number of cells of each target cell type in each grid prism
+    for (target_cell_type in target_cell_types) {
+      cell_type_list[[target_cell_type]] <- append(cell_type_list[[target_cell_type]], 
+                                                   sum(data_temp$Cell.Type == target_cell_type))
+    }
+    
+    grid_prism_entropies <- c(grid_prism_entropies, grid_prism_entropy)
+    
+  }
+  
+  result <- data.frame(row.names = seq(n_grid_prisms))
+  
+  ## Add column for each target cell type representing the number of cells in each grid prism
+  for (target_cell_type in target_cell_types) {
+    result[[target_cell_type]] <- cell_type_list[[target_cell_type]]
+  }
+  
+  ## Add column for total cell count for each grid prism
+  result$Total <- apply(result, 1, sum)
+  
+  ## Add entropy column
+  result$Entropy = grid_prism_entropies
+  
+  ## Plot
+  if (plot_image) {
+
+    plot_data <- result
+    
+    ## Place a dot at the center of each grid prism to represent entropy
+    ## Use the grid prism number to figure out their location...
+    plot_data$x <- ((seq(n_grid_prisms) - 1) %% n_split + 0.5) * d_row
+    plot_data$y <- (floor(((seq(n_grid_prisms) - 1) %% (n_split)^2) / n_split) + 0.5) * d_col
+    plot_data$z <- (floor((seq(n_grid_prisms) - 1) / (n_split^2)) + 0.5) * d_lay
+    
+    ## Color of each dot is related to its entropy
+    pal <- colorRampPalette(hcl.colors(n = 5, palette = "Terrain", rev = TRUE))
+    plot_data$order = findInterval(plot_data$Entropy, sort(plot_data$Entropy), left.open = TRUE) + 1
+    
+    plot3d(plot_data$x, plot_data$y, plot_data$z, size = 10, col = pal(nrow(plot_data))[plot_data$order])    
+    
+  }
+
+  return (result)
 }
