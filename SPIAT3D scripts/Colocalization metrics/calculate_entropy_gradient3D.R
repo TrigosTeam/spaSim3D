7ff1c5d2d33ca2150079e8983ff7fbe2c42ecdb1
dave.le23 <david.le@petermac.org>
2024-04-04 11:19:44 +1100

    Added option to choose whether to plot entropy gradient. Going to stick with average entropy rather than summed entropy.

--- SPIAT3D scripts/Colocalization metrics/calculate_entropy_gradient3D.R
+++ SPIAT3D scripts/Colocalization metrics/calculate_entropy_gradient3D.R
@@ -2,24 +2,27 @@ calculate_entropy_gradient3D <- function(data,
                                          radii,
                                          reference_cell_type,
                                          target_cell_types,
-                                         feature_colname = "Cell.Type") {
+                                         feature_colname = "Cell.Type",
+                                         plot_image = TRUE) {
   
   entropy_gradient <- list()
   entropy_mean <- c()
   
   for (radius in seq(1, radii, 0.5)) {
-    entropy_data <- calculate_entropy(data,
-                                      radius,
-                                      reference_cell_type,
-                                      target_cell_type,
-                                      feature_colname)
+    entropy_data <- calculate_entropy3D(data,
+                                        radius,
+                                        reference_cell_type,
+                                        target_cell_types,
+                                        feature_colname)
     
-    entropy_gradient[[paste("radius = ", radius)]] <- entropy_data
+    entropy_gradient[[paste(radius)]] <- entropy_data
     entropy_mean <- append(entropy_mean, mean(entropy_data$Entropy))
     
   }
   
-  plot(seq(1, radii, 0.5), entropy_mean, type = "l", xlab = "Radius", ylab = "Entropy mean")
+  if (plot_image) {
+    plot(seq(1, radii, 0.5), entropy_mean, type = "l", xlab = "Radius", ylab = "Entropy Mean")
+  }
   
   return(entropy_gradient)
   
