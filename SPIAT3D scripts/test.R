aa3766e4c737546f5cadb2cf2832a1d310967700
dave.le23 <david.le@petermac.org>
2024-04-07 20:55:30 +1000

    Junk

--- SPIAT3D scripts/test.R
+++ SPIAT3D scripts/test.R
@@ -1,5 +1,5 @@
-grid_m <- grid_metrics(formatted_spatial_data, calculate_entropy, 5,
-                       cell_types_of_interest=c("Tumour","CD4 T"), feature_colname = "Cell.Type")
+df$Cell.Type
 
-grid_percents <- calculate_percentage_of_grids(grid_m, threshold = 0.75, above = TRUE)
-                     
+?match
+
+df$Color <- color_order[match(df$Cell.Type, cell_type_order)]
