3b33066bd2c553f17b580a4e17e9460e559e03b4
dave.le23 <david.le@petermac.org>
2024-04-07 20:55:23 +1000

    Fixed bug that included cells that we wanted to exclude. Other minor changes too.

--- SPIAT3D scripts/Basic metrics/calculate_cell_proportions3D.R
+++ SPIAT3D scripts/Basic metrics/calculate_cell_proportions3D.R
@@ -11,11 +11,12 @@ calculate_cell_proportions3D <- function(data,
   
   # Creates frequency/bar plot of all cell types in the entire image
   cell_proportions <- as.data.frame(table(data[, feature_colname]))
-  names(cell_proportions) <- c("Cell_Type", 'Frequency')
+  names(cell_proportions) <- c("Cell.Type", 'Frequency')
   
   # Exclude any cell types not wanted
   if (!is.null(cell_types_to_exclude)) {
-    cell_proportions <- cell_proportions[cell_proportions$Cell_Type %in% cell_types_to_exclude, ]
+
+    cell_proportions <- cell_proportions[!(cell_proportions$Cell.Type %in% cell_types_to_exclude), ]
   }
   
   # Find proportion of each cell type against all cells
@@ -24,17 +25,17 @@ calculate_cell_proportions3D <- function(data,
     cell_type_frequency_total <- sum(cell_proportions$Frequency)
     
     cell_proportions$Proportion <- cell_proportions$Frequency / cell_type_frequency_total
-    cell_proportions$Percentage <- cell_proportions$Proportion*100
-    cell_proportions$Proportion_Name <- paste(cell_proportions[['Cell_Type']], "Total", sep="/")  
+    cell_proportions$Percentage <- cell_proportions$Proportion * 100
+    cell_proportions$Proportion_Name <- "/Total"
   }
   # Find proportion of each cell type against the chosen reference cell types
   else {
     # Get frequency total for chosen reference cells
-    cell_type_frequency_total <- sum(cell_proportions$Frequency[cell_proportions[['Cell_Type']] %in% reference_cell_types])
+    cell_type_frequency_total <- sum(cell_proportions$Frequency[cell_proportions[['Cell.Type']] %in% reference_cell_types])
   
     cell_proportions$Proportion <- cell_proportions$Frequency/cell_type_frequency_total
-    cell_proportions$Percentage <- cell_proportions$Proportion*100
-    cell_proportions$Proportion_Name <- paste(cell_proportions[['Cell_Type']], "Reference", sep="/")   
+    cell_proportions$Percentage <- cell_proportions$Proportion * 100
+    cell_proportions$Proportion_Name <- "/Custom"  
     cell_proportions$Reference <- paste(reference_cell_types, collapse=",")
   }
   
@@ -42,7 +43,7 @@ calculate_cell_proportions3D <- function(data,
   cell_proportions <- cell_proportions[rev(order(cell_proportions$Proportion)), ]
   
   if (plot.image) {
-    g <- ggplot(cell_proportions, aes(x=Cell_Type, y=Percentage)) +
+    g <- ggplot(cell_proportions, aes(x=Cell.Type, y=Percentage)) +
       geom_bar(stat='identity') + theme_bw()
     methods::show(g)
   }
