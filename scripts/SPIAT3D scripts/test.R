97df56a0d98466393f4788ccf3849818bc2c60f3
dave.le23 <david.le@petermac.org>
2024-04-10 10:13:32 +1000

    Junk

--- scripts/SPIAT3D scripts/test.R
+++ scripts/SPIAT3D scripts/test.R
@@ -1,7 +1,39 @@
-df$Cell.Type
+length <- width <- height <- 100
+n_cells_inflated <- 12000
+min_d <- 2
 
-?match
+# Use poisson distribution to sample points
+pois_df <- poisson_distribution3D(n_cells = n_cells_inflated, 
+                                  length = length, 
+                                  width = width, 
+                                  height = height)
 
-df$Color <- color_order[match(df$Cell.Type, cell_type_order)]
 
-print("Hello world")
+pois_df_distances <- dbscan::frNN(pois_df[ , c(1, 2, 3)], 
+                                  eps = min_d,
+                                  query = NULL, 
+                                  sort = FALSE)
+
+pois_df_distances_ids <- pois_df_distances$id
+
+n_cells <- nrow(pois_df)
+i <- 1
+
+chosen_cells <- seq(length(pois_df_distances_ids))
+
+
+while (i < n_cells) {
+
+  cell <- chosen_cells[i]
+  cells_to_remove <- pois_df_distances_ids[[cell]]
+  n_cells_to_remove <- length(cells_to_remove)
+  
+  if (n_cells_to_remove != 0) {
+    chosen_cells <- chosen_cells[-(cells_to_remove)]
+    n_cells <- n_cells - length(pois_df_distances_ids[[cell]])
+  }
+  i <- i + 1 
+}
+
+
+pois_df <- pois_df[chosen_cells, ]
