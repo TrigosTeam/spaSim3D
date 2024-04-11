8d4858a8af45cf9128700f8d31fa0d5c18f8bbd4
dave.le23 <david.le@petermac.org>
2024-04-11 12:07:49 +1000

    New function to calculate grid_metrics but looking at cell proportions instead of entropy.

---
+++ scripts/SPIAT3D scripts/Spatial heterogeneity metrics/determine_proportion_grid_metrics3D.R
@@ -0,0 +1,142 @@
+determine_cell_proportion_grid_metrics3D <- function(data, 
+                                                     n_split,
+                                                     reference_cell_type,
+                                                     target_cell_type,
+                                                     feature_colname = "Cell.Type",
+                                                     size = NULL,
+                                                     plot_image = TRUE) {
+  
+  # If the columns are not correct, give error
+  required_colnames <- c("Cell.X.Position", 
+                         "Cell.Y.Position", 
+                         "Cell.Z.Position", 
+                         feature_colname)
+  
+  missing_colnames <- setdiff(required_colnames,
+                              colnames(data))
+  
+  if (length(missing_colnames) > 0) {
+    stop(paste(paste(missing_colnames, collapse = ', '),
+               "are missing as column names in your data")) 
+  }
+  
+  # Check if n_split is numeric
+  if (!is.numeric(n_split)) {
+    stop(paste(n_split, " n_split is not of type 'numeric'"))
+  }
+  
+  # Check if reference_cell_type is found in the data
+  if (!reference_cell_type %in% unique(data[[feature_colname]])) {
+    stop(paste(reference_cell_type, " reference_cell_type doesn't exist in data"))
+  }
+  # Check if target_cell_type is found in the data  
+  if (!target_cell_type %in% unique(data[[feature_colname]])) {
+    stop(paste(target_cell_type, " target_cell_type doesn't exist in data"))
+  }
+  
+  
+  
+  
+  ## Get dimensions of the window
+  length <- round(max(data$Cell.X.Position) - min(data$Cell.X.Position))
+  width  <- round(max(data$Cell.Y.Position) - min(data$Cell.Y.Position))
+  height <- round(max(data$Cell.Z.Position) - min(data$Cell.Z.Position))
+  
+  ## Get distance of row, col and lay
+  d_row <- length / n_split
+  d_col <- width / n_split
+  d_lay <- height / n_split
+  
+  ## Figure out which 'grid prism number' each cell is inside
+  data$Prism.Num <- floor(data$Cell.X.Position / d_row) +
+    floor(data$Cell.Y.Position / d_col) * n_split + 
+    floor(data$Cell.Z.Position / d_lay) * n_split^2 + 1
+  
+  ## Calculate cell_proportions for each grid prism
+  n_grid_prisms <- n_split^3
+  cell_type_list <- vector(mode = 'list', length = 2)
+  grid_prism_cell_proportions <- c()
+  
+  for (grid_prism_num in seq(n_grid_prisms)) {
+    
+    ## Get data of cells in the current grid_prism
+    data_temp <- data[data$Prism.Num == grid_prism_num, ]
+    
+    
+    ## Get cell_proportion: n_target_cells / (n_target_cells + n_reference_cells)
+    n_target_cells <- sum(data_temp[[feature_colname]] == target_cell_type)
+    n_reference_cells <- sum(data_temp[[feature_colname]] == reference_cell_type)
+    
+    ## Case when there are no target or reference cell, result is NA
+    if (n_target_cells == 0 && n_reference_cells == 0) {
+      grid_prism_cell_proportion <- NA  
+    }
+    else {
+      grid_prism_cell_proportion <- n_target_cells / (n_target_cells + n_reference_cells)
+    }
+    
+    ## Get number of cells of reference cell type and target cell type in each grid prism
+    cell_type_list[[reference_cell_type]] <- append(cell_type_list[[reference_cell_type]], 
+                                                 sum(data_temp[[feature_colname]] == reference_cell_type))
+    cell_type_list[[target_cell_type]] <- append(cell_type_list[[target_cell_type]], 
+                                                 sum(data_temp[[feature_colname]] == target_cell_type))
+    
+    grid_prism_cell_proportions <- c(grid_prism_cell_proportions, grid_prism_cell_proportion)
+    
+  }
+  
+  result <- data.frame(row.names = seq(n_grid_prisms))
+  
+  ## Add column for reference cell type and target cell type representing the number of cells in each grid prism
+  result[[reference_cell_type]] <- cell_type_list[[reference_cell_type]]
+  result[[target_cell_type]] <- cell_type_list[[target_cell_type]]
+  
+  ## Add column for total cell count for each grid prism
+  result$Total <- apply(result, 1, sum)
+  
+  ## Add cell proportion column
+  result$Proportion = grid_prism_cell_proportions
+  
+  ## Plot
+  if (plot_image) {
+    
+    # Check if size is numeric or not
+    if (!is.numeric(size)) {
+      stop(paste(size, " size is not numeric"))
+    }
+    
+    plot_data <- result
+    
+    ## Place a dot at the center of each grid prism to represent cell proportion
+    ## Use the grid prism number to figure out their location...
+    plot_data$x <- ((seq(n_grid_prisms) - 1) %% n_split + 0.5) * d_row
+    plot_data$y <- (floor(((seq(n_grid_prisms) - 1) %% (n_split)^2) / n_split) + 0.5) * d_col
+    plot_data$z <- (floor((seq(n_grid_prisms) - 1) / (n_split^2)) + 0.5) * d_lay
+    
+    ## Color of each dot is related to its cell proportion
+    pal <- colorRampPalette(hcl.colors(n = 5, palette = "Red-Blue", rev = TRUE))
+    
+    
+    ## Add size column and for NA cell proportion values, make the size small
+    plot_data$size <- ifelse(is.na(plot_data$Proportion), 3, size)
+    
+    fig <- plot_ly(plot_data,
+                   type = "scatter3d",
+                   mode = 'markers',
+                   x = ~x,
+                   y = ~y,
+                   z = ~z,
+                   color = ~Proportion,
+                   colors = pal(nrow(plot_data)),
+                   marker = list(size = ~size))
+    
+    fig <- fig %>% layout(scene = list(xaxis = list(title = 'x'),
+                                       yaxis = list(title = 'y'),
+                                       zaxis = list(title = 'z')))
+    
+    print(fig)
+    
+  }
+  
+  return (result)
+}
