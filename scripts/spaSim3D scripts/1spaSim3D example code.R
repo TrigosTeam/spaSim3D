c08b39c063e360877ca6a589fa4da1b0e62d456b
dave.le23 <david.le@petermac.org>
2024-04-11 11:59:55 +1000

    changed parameters

--- scripts/spaSim3D scripts/1spaSim3D example code.R
+++ scripts/spaSim3D scripts/1spaSim3D example code.R
@@ -1,6 +1,7 @@
 ### Add Cell.ID
-chosen_bg <- bg_spheres
+chosen_bg <- bg_mix
 chosen_bg$Cell.ID <- (paste("Cell_", seq(nrow(chosen_bg)), sep="")) ## adding Cell.ID column
+data <- chosen_bg
 
 
 ###-------------------------------------------------------------------------###
