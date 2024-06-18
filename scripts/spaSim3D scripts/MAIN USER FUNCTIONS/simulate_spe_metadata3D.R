cb16a6b910193708144b21b5970337d117d851c0
dave.le23 <david.le@petermac.org>
2024-06-18 10:06:33 +1000

    Minor fix.

--- scripts/spaSim3D scripts/MAIN USER FUNCTIONS/simulate_spe_metadata3D.R
+++ scripts/spaSim3D scripts/MAIN USER FUNCTIONS/simulate_spe_metadata3D.R
@@ -1,5 +1,5 @@
 simulate_spe_metadata3D <- function(spe_metadata) {
-
+  
   # First element should contain background metadata
   bg_metadata <- spe_metadata[[1]]
   if (bg_metadata$background_type == "random") {
@@ -23,11 +23,14 @@ simulate_spe_metadata3D <- function(spe_metadata) {
   spe <- simulate_mixing3D(spe,
                            bg_metadata$cell_types,
                            bg_metadata$cell_proportions)
-   
+  
+  ### If there is only background metadata, we are done
+  if (length(spe_metadata) == 1) return(spe)
+  
   
   ### All other elements should help to simulate clusters 
-  for (i in seq(length(spe_metadata) - 1)) {
-    cluster_metadata <- spe_metadata[[i + 1]]
+  for (i in 2:length(spe_metadata)) {
+    cluster_metadata <- spe_metadata[[i]]
     if (cluster_metadata$cluster_type == "regular") {
       spe <- simulate_clusters3D(spe, list(cluster_metadata))
     }
