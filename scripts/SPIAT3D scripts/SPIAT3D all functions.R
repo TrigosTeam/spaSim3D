9e8875d8cab7d536bad38a10abfec9f4985ccb81
LeDavid <dle@papr-res-compute207.unix.petermac.org.au>
2024-07-11 16:13:05 +1000

    Add plot_cells3D function to SPIAT-3D all functions script.

--- scripts/SPIAT3D scripts/SPIAT3D all functions.R
+++ scripts/SPIAT3D scripts/SPIAT3D all functions.R
@@ -2262,3 +2262,59 @@ calculate_center_of_clusters3D <- function(spe, cluster_colname) {
 
 
 
+plot_cells3D <- function(spe,
+                         plot_cell_types = NULL,
+                         plot_colours = NULL,
+                         feature_colname = "Cell.Type") {
+  
+  ## Convert spe object to data frame
+  df <- data.frame(spatialCoords(spe), "Cell.Type" = spe[[feature_colname]])
+  
+  ## If no cell types chosen, use all cell types found in data frame
+  if (is.null(plot_cell_types)) {
+    plot_cell_types <- unique(df[["Cell.Type"]])
+  }
+  ## If cell types have been chosen, check they are found in the spe object
+  unknown_cell_types <- setdiff(plot_cell_types, spe[[feature_colname]])
+  if (length(unknown_cell_types) != 0) {
+    stop(paste("The following plot_cell_types are not found in the spe object:\n   ",
+               paste(unknown_cell_types, collapse = ", ")))
+  }
+  
+  ## If no colours inputted, use rainbow palette
+  if (is.null(plot_colours)) {
+    plot_colours <- rainbow(length(plot_cell_types))
+  }
+  
+  ## User inputs mismatching cell types and colours
+  if (length(plot_cell_types) != length(plot_colours)) {
+    stop("Length of plot_cell_types is not equal to length of plot_colours")
+  }
+  
+  ## Factor for feature column
+  df[, "Cell.Type"] <- factor(df[, "Cell.Type"],
+                              levels = plot_cell_types)
+  
+  ## Plot
+  fig <- plot_ly(df,
+                 type = "scatter3d",
+                 mode = 'markers',
+                 x = ~Cell.X.Position,
+                 y = ~Cell.Y.Position,
+                 z = ~Cell.Z.Position,
+                 color = ~Cell.Type,
+                 colors = plot_colours,
+                 marker = list(size = 2))
+  
+  fig <- fig %>% layout(scene = list(xaxis = list(title = 'x'),
+                                     yaxis = list(title = 'y'),
+                                     zaxis = list(title = 'z')))
+  
+  fig <- fig %>% layout(scene = list(xaxis = list(title = '', showgrid = T, showaxeslabels = F, showticklabels = F),
+                                     yaxis = list(title = '', showgrid = T, showaxeslabels = F, showticklabels = F),
+                                     zaxis = list(title = '', showgrid = T, showaxeslabels = F, showticklabels = F)))
+  
+  
+  return (fig)
+}
+
