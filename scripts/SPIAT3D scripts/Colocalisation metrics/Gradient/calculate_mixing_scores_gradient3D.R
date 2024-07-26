304c71cdffea7d6ec6663ca9f63282e8e28ff1cb
LeDavid <dle@rstudio-4.4.1>
2024-07-26 15:46:27 +1000

    Added MS gradient graph.

--- scripts/SPIAT3D scripts/Colocalisation metrics/Gradient/calculate_mixing_scores_gradient3D.R
+++ scripts/SPIAT3D scripts/Colocalisation metrics/Gradient/calculate_mixing_scores_gradient3D.R
@@ -29,17 +29,27 @@ calculate_mixing_scores_gradient3D <- function(spe,
   result$radius <- seq(radii)
   
   if (plot_image) {
-    plot_result <- result
-    plot_result$expected_normalised_mixing_score <- 1
-    plot_result <- reshape2::melt(plot_result, "radius", c("normalised_mixing_score", "expected_normalised_mixing_score"))
+    plot_result1 <- result
+    plot_result1$expected_normalised_mixing_score <- 1
+    plot_result1 <- reshape2::melt(plot_result1, "radius", c("normalised_mixing_score", "expected_normalised_mixing_score"))
     
-    fig <- ggplot(plot_result, aes(x = radius, y = value, color = variable)) +
+    fig1 <- ggplot(plot_result1, aes(x = radius, y = value, color = variable)) +
       geom_line() +
       labs(x = "Radius", y = "Normalised mixing score (NMS)") +
       scale_colour_discrete(name = "", labels = c("Observed NMS", "Expected CSR NMS")) +
       theme_bw()
     
-      methods::show(fig)
+    plot_result2 <- reshape2::melt(result, "radius", "mixing_score")
+    
+    fig2 <- ggplot(plot_result2, aes(x = radius, y = value, color = variable)) +
+      geom_line() +
+      labs(x = "Radius", y = "Mixing score (MS)") +
+      scale_colour_discrete(name = "", labels = c("Observed MS")) +
+      theme_bw()
+    
+    combined_fig <- plot_grid(fig1, fig2, nrow = 2)
+    
+    methods::show(combined_fig)
   }
   
   return(result)
