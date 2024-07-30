0c62e6deeae8d4a2c5a11f63389e6becb78243a5
LeDavid <dle@rstudio-4.4.1>
2024-07-30 14:16:25 +1000

    Updated spatial heterogeneity functions.

--- scripts/SPIAT3D scripts/SPIAT3D all functions.R
+++ scripts/SPIAT3D scripts/SPIAT3D all functions.R
@@ -1136,6 +1136,72 @@ get_spe_grid_metrics3D <- function(spe,
 
 
 
+calculate_cell_proportion_grid_metrics3D <- function(spe, 
+                                                     n_splits,
+                                                     reference_cell_types,
+                                                     target_cell_types,
+                                                     feature_colname = "Cell.Type",
+                                                     plot_image = TRUE) {
+  
+  if (is.null(spe[[feature_colname]])) stop(paste("No column called", feature_colname, "found in spe object"))
+  
+  ## Check reference_cell_types are found in the spe object
+  unknown_cell_types <- setdiff(reference_cell_types, spe[[feature_colname]])
+  if (length(unknown_cell_types) != 0) {
+    stop(paste("The following cell types in reference_cell_types are not found in the spe object:\n   ",
+               paste(unknown_cell_types, collapse = ", ")))
+  }
+  ## Check target_cell_types are found in the spe object
+  unknown_cell_types <- setdiff(target_cell_types, spe[[feature_colname]])
+  if (length(unknown_cell_types) != 0) {
+    stop(paste("The following cell types in target_cell_types are not found in the spe object:\n   ",
+               paste(unknown_cell_types, collapse = ", ")))
+  }
+  # Check if there is intersection between reference_cell_types and target_cell_types
+  if (length(intersect(reference_cell_types, target_cell_types)) > 0) {
+    stop("Cannot have same cells in both reference_cell_types and target_cell_types")
+  }
+  
+  # Add grid metrics to spe
+  spe <- get_spe_grid_metrics3D(spe, n_splits, feature_colname)
+  
+  # Get grid_prism_cell_matrix from spe
+  grid_prism_cell_matrix <- spe@metadata$grid_metrics$grid_prism_cell_matrix
+  
+  ## Define data frame which contains all results
+  n_grid_prisms <- n_splits^3
+  result <- data.frame(row.names = seq(n_grid_prisms))
+  
+  # Fill in the result data frame
+  if (length(reference_cell_types) == 1) {
+    result$reference <- grid_prism_cell_matrix[[reference_cell_types]]
+  }
+  else {
+    result$reference <- rowSums(grid_prism_cell_matrix[ , reference_cell_types])
+  }
+  if (length(target_cell_types) == 1) {
+    result$target <- grid_prism_cell_matrix[[target_cell_types]]
+  }
+  else {
+    result$target <- rowSums(grid_prism_cell_matrix[ , target_cell_types])
+  }
+  result$total <- result$reference + result$target
+  result$proportion <- result$target / result$total
+  
+  # Add grid_prism_coordinates info to result
+  result <- cbind(result, spe@metadata$grid_metrics$grid_prism_coordinates)
+  
+  ## Plot
+  if (plot_image) {
+    fig <- plot_grid_metrics_continuous3D(result, "proportion")
+    methods::show(fig)
+  }
+  
+  return(result)
+}
+
+
+
 calculate_entropy_grid_metrics3D <- function(spe, 
                                              n_splits,
                                              cell_types_of_interest,
@@ -1175,60 +1241,6 @@ calculate_entropy_grid_metrics3D <- function(spe,
     return(entropy)
   }
   result$entropy <- apply(df_props, 1, calculate_entropy)
-  
-  # Add grid_prism_coordinates info to result
-  result <- cbind(result, spe@metadata$grid_metrics$grid_prism_coordinates)
-  
-  ## Plot
-  if (plot_image) {
-    fig <- plot_grid_metrics_continuous3D(result, "entropy")
-    methods::show(fig)
-  }
-  
-  return(result)
-}
-
-
-
-calculate_entropy_grid_metrics3D <- function(spe, 
-                                             n_splits,
-                                             cell_types_of_interest,
-                                             feature_colname = "Cell.Type",
-                                             plot_image = TRUE) {
-  
-  if (is.null(spe[[feature_colname]])) stop(paste("No column called", feature_colname, "found in spe object"))
-  
-  ## If cell types have been chosen, check they are found in the spe object
-  unknown_cell_types <- setdiff(cell_types_of_interest, unique(spe[[feature_colname]]))
-  if (length(unknown_cell_types) != 0) {
-    stop(paste("The following cell types in cell_types_of_interest are not found in the spe object:\n   ",
-               paste(unknown_cell_types, collapse = ", ")))
-  }
-  
-  # Add grid metrics to spe
-  spe <- get_spe_grid_metrics3D(spe, n_splits, feature_colname)
-  
-  # Get grid_prism_cell_matrix from spe
-  grid_prism_cell_matrix <- spe@metadata$grid_metrics$grid_prism_cell_matrix
-  
-  ## Define data frame which contains all results
-  n_grid_prisms <- n_splits^3
-  result <- data.frame(row.names = seq(n_grid_prisms))
-  
-  for (cell_type in cell_types_of_interest) {
-    result[[cell_type]] <- grid_prism_cell_matrix[[cell_type]]
-  }
-  result$total <- rowSums(result)
-  
-  df_props <- result[ , cell_types_of_interest] / result$total
-  
-  calculate_entropy <- function(x) {
-    entropy <- -1 * sum(x * ifelse(is.infinite(log(x, length(x))), 0, log(x, length(x))))
-    return(entropy)
-  }
-  
-  result$entropy <- -1 * rowSums(df_props * ifelse(is.infinite(log(df_props, length(cell_types_of_interest))), 0, log(df_props, length(cell_types_of_interest))))
-  result[apply(df_props, 1, function(x) 1 %in% x), "entropy"] <- 0
   
   # Add grid_prism_coordinates info to result
   result <- cbind(result, spe@metadata$grid_metrics$grid_prism_coordinates)
