bbe3aa804f8109703f0c72742e801023944f42fc
David Le <dle@nid002813.hostmgmt.cm.setonix.pawsey.org.au>
2026-05-30 12:55:03 +0800

    Added roxygen commenting to simulate_cylinder_ring function

--- R/simulate_cylinder_ring.R
+++ R/simulate_cylinder_ring.R
@@ -1,3 +1,72 @@
+#' @title Simulate a cylinder cluster with a ring in spaSim3D
+#'
+#' @description This functions simulates a cylinder cluster with a ring onto an 
+#'     existing SpatialExperiment object. The parameters of the cylinder and its 
+#'     ring are fully customisable by the user.
+#' 
+#' @param spe A SpatialExperiment object containing 3D spatial information for 
+#'     the cells. Should be generated using the output of one of the following
+#'     functions: simulate_random_background_cells3D, 
+#'     simulate_ordered_background_cells3D,
+#'     simulate_mixing3D or any of the other simulate_* functions. This is 
+#'     because the metadata of the SpatialExperiment object needs to already 
+#'     contain spaSim3D specific data relating to the background of the 
+#'     SpatialExperiment object, and any clusters.
+#' @param ring_properties A list containing the properties of the cylinder 
+#'     cluster and ring desired. The list should contain the following elements:
+#'     "shape": Must be equal to the character "cylinder".
+#'     "cluster_cell_types": A character vector representing the cell types that 
+#'         make up the cluster. E.g. c("Tumour", "Immune").
+#'     "cluster_cell_proportions": A numeric vector representing the proportion 
+#'         of each cell type in the cluster. Its elements must each be 
+#'         greater than 0, sum to 1 and the vector must be the same length as
+#'         "cluster_cell_types". E.g. c(0.6, 0.4) corresponds to a cluster made
+#'         up of 60% Tumour and 40% Immune.
+#'     "radius": A positive number representing the radius of the cylinder.
+#'     "start_loc": A numerical vector of length 3 representing the starting
+#'         x,y,z coordinate of the cylinder. E.g. (0, 0, 0).
+#'     "end_loc": A numerical vector of length 3 representing the ending x,y,z 
+#'         coordinate of the cylinder. E.g. (20, 20, 20).
+#'     "ring_cell_types": A character vector representing the cell types 
+#'         that make up the ring. E.g. c("Immune1", "Immune2", "Immune3").
+#'     "ring_cell_proportions": A numeric vector representing the 
+#'         proportion of each cell type in the ring. Its elements must each be 
+#'         greater than 0, sum to 1 and the vector must be the same length as
+#'         "ring_cell_types". E.g. c(0.3, 0.4, 0.3) corresponds to an
+#'         ring made up of 30% Immune1, 40% Immune2 and 30% Immune3.
+#'     "ring_width": A positive number representing the width of the ring.
+#'
+#' @return The same 3D SpatialExperiment object used as input for spe, updated
+#'     with the new cylinder with ring cluster and the corresponding metadata.
+#'
+#' @examples
+#' # Simulate background
+#' bg_r <- simulate_random_background_cells3D(n_cells = 10000,
+#'                                            length = 100,
+#'                                            width = 100,
+#'                                            height = 100,
+#'                                            minimum_distance_between_cells = 0.5,
+#'                                            background_cell_type = "Others",
+#'                                            plot_image = TRUE)
+#'                                            
+#' # Simulate clusters
+#' cylinder_ring_cluster <- simulate_cylinder_ring(bg_r,
+#'                                                 ring_properties = list(
+#'                                                   shape = "cylinder",
+#'                                                   cluster_cell_types = c("Endothelial", "Others"),
+#'                                                   cluster_cell_proportions = c(0.95, 0.05),
+#'                                                   radius = 10,
+#'                                                   start_loc = c(0, 0, 0),
+#'                                                   end_loc   = c(20, 20, 100),
+#'                                                   ring_cell_types = c("Immune1", "Others"),
+#'                                                   ring_cell_proportions = c(0.85, 0.15),
+#'                                                   ring_width = 5
+#'                                                 ))
+#' # Plot
+#' plots_cells3D(cylinder_ring_cluster)
+#'                                             
+#' @export
+
 simulate_cylinder_ring <- function(spe, 
                                    ring_properties) {
   
