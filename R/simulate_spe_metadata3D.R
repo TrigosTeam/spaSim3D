ae0cbf8912b303effe6b53286fcd861227dc6fa0
David Le <dle@nid001793.hostmgmt.cm.setonix.pawsey.org.au>
2026-05-31 08:22:21 +0800

    Added roxygen commenting for simulate_spe_metadata function.

--- R/simulate_spe_metadata3D.R
+++ R/simulate_spe_metadata3D.R
@@ -1,3 +1,40 @@
+#' @title Simulate a SpatialExperiment object with spaSim3D metadata.
+#'
+#' @description This function generates a SpatialExperiment object with 
+#'     parameters specified with inputted spaSim3D metadata. By doing so, it
+#'     simulates a 3D tissue, beginning with the background cells of the tissue,
+#'     then adding cell clusters to the tissue.
+#' 
+#' @param spe_metadata A list containing the metadata to generate the
+#'     SpatialExperiment object. This is generated from the output of the 
+#'     spaSim3D functions 'spe_metadata_background_template' and
+#'    'spe_metadata_cluster_template'.
+#' @param plot_image A logical indicating whether to plot 3D spatial data with 
+#'     the added metadata. Defaults to TRUE.
+#' @param plot_cell_types A string vector specifying the cell types to plot. If 
+#'     NULL, all cell types in the `feature_colname` column will be considered. 
+#'     Defaults to NULL.
+#' @param plot_colours A string vector specifying the colours of the cell types
+#'     when plotting. Must match the number of cell types specified in 
+#'     `plot_cell_types`. If NULL, the viridis color pallete will be used. 
+#'     Defaults to NULL.
+#'
+#' @return A SpatialExperiment object containing the cells of the simulated 3D
+#'     tissue and including the inputted metadata.
+#'
+#' @examples
+#' # Get background metadata
+#' bg_metadata <- spe_metadata_background_template("random")
+#' 
+#' # Get cluster metadata (using background metadata as background)
+#' cluster_metadata <- spe_metadata_cluster_template("regular", "sphere", bg_metadata)
+#' cluster_metadata <- spe_metadata_cluster_template("ring", "ellipsoid", cluster_metadata)
+#' 
+#' # Get spe from cluster metadata
+#' spe_clusters <- simulate_spe_metadata3D(cluster_metadata)
+#' 
+#' @export
+
 simulate_spe_metadata3D <- function(spe_metadata, 
                                     plot_image = TRUE, 
                                     plot_cell_types = NULL,
